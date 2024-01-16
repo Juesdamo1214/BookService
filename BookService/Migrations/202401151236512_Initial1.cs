475aab790b9b9e48a592b9c12c790480d11425aa
Juesdamo1214 <juesdamo1412@gmail.com>
2024-01-16 09:40:56 -0500

    Finalizacion de ASP.NET Web API 2

---
+++ BookService/Migrations/202401151236512_Initial1.cs
@@ -0,0 +1,18 @@
+﻿namespace BookService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Authors", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Authors", "Name", c => c.String());
+        }
+    }
+}
